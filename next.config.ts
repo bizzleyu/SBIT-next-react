119611f5869fe1eac28c513accfc348014f96601
weihong.yu <yuweihong@ywgroup.cc>
2026-04-10 13:00:12 +0800

    update: 自动构建

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/SBTI-test',
+  basePath: '/SBIT-next-react',
   images: { unoptimized: true },
 };
 
